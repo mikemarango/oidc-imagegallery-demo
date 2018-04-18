6bfe124a3d801e52da7f3948461b93b1262e8615
Michael Marango <mike.marango@gmail.com>
2018-04-18 08:29:54 -0700

    setup database

---
+++ ImageGallery/ImageGallery.Data/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageGallery.Data
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string  FileName { get; set; }
+        public string OwnerId { get; set; }
+    }
+}
