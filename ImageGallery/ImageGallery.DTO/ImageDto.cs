aae32717dca24413f1482e3733f62ae39b1e3148
Michael Marango <mike.marango@gmail.com>
2018-04-18 08:37:08 -0700

    added dtos

--- ImageGallery/ImageGallery.DTO/ImageDto.cs
+++ ImageGallery/ImageGallery.DTO/ImageDto.cs
@@ -2,7 +2,7 @@
 
 namespace ImageGallery.DTO
 {
-    public class Class1
+    public class ImageDto
     {
     }
 }
---
+++ ImageGallery/ImageGallery.DTO/ImageUpdaterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ImageGallery.DTO
+{
+    public class ImageUpdaterDto
+    {
+        [Required, MaxLength(150)]
+        public string Title { get; set; }
+    }
+}
